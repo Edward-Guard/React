3fc6fd186f045dcf180224dc5c6a82d6dba428a6
Edward Silva <107967519+Edward-Guard@users.noreply.github.com>
2023-02-20 16:39:34 -0300

    main

--- piano/src/App.js
+++ piano/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Import your audio file
+import Do from "./Notas-Piano/do.wav";
+import Re from "./Notas-Piano/Re.wav"
+import Mi from "./Notas-Piano/Mi.wav"
+import Fa from "./Notas-Piano/Fa.wav"
+import La from "./Notas-Piano/La.wav"
+import Sol from "./Notas-Piano/Sol.wav"
+import Si from "./Notas-Piano/Si.wav"
+
+ 
+
+class App extends Component {
+  // Create state
+  state = {
+
+    // Get audio file in a variable
+    Do: new Audio(Do),
+    Re: new Audio(Re),
+    Mi: new Audio(Mi),
+    Fa: new Audio(Fa),
+    La: new Audio(La),
+    Sol: new Audio(Sol),
+    Si: new Audio(Si),
+
+    // Set initial state of song
+    isPlaying: false,
+  };
+
+  // Main function to handle both play and pause operations
+  
+
+  Do=() =>{
+    this.state.Do.play()
+  }
+
+  Re=() =>{
+    this.state.Re.play()
+  }
+
+  Mi=() =>{
+    this.state.Mi.play()
+  }
+
+  Fa=() =>{
+    this.state.Fa.play()
+  }
+
+  Sol=() =>{
+    this.state.Sol.play()
+  }
+
+  La=() =>{
+    this.state.La.play()
+  }
+
+  Si=() =>{
+    this.state.Si.play()
+  }
+
+  render() {
+    return (
+      <div className="teclado">
+        <button className="nota" onClick={this.Do}> Dó </button>
+        <button className="nota" onClick={this.Re}>Ré</button>
+        <button className="nota" onClick={this.Mi}>Mi</button>
+        <button className="nota" onClick={this.Fa}>Fá</button>
+        <button className="nota" onClick={this.Sol}>Sol</button>
+        <button className="nota" onClick={this.La}>LA</button>
+        <button className="nota" onClick={this.Si}>Si</button>
+      </div>
+    );
+  }
 }
 
 export default App;
